0521a89bf9aa16e1e6172cb6f697a32ced87fb22
jasmyn williams <jwil1542@students.kennesaw.edu>
2025-12-07 02:52:02 -0600

    Add background image to the game screen
    Load and set the background image for the game.

--- correctsnakegame.py
+++ correctsnakegame.py
@@ -68,6 +68,8 @@ class apple():
 class game():
     def __init__(self,speed, size=1):
         self.screen = pg.display.set_mode((800,450))
+        self.background = pg.image.load("menu_background.jpg").convert()
+        self.background = pg.transform.scale(self.background, (800, 450))
         pg.display.set_caption('Snake Game')
         self.snake = snake(speed, size)
         self.blocks = []
@@ -284,7 +286,7 @@ class startmenu():
         self.make_text((pos[0]+pos[2]/2), (pos[1]+pos[3]/2), text, a = True, size=textsize)
     def mainloop(self):
         while 1:
-            self.screen.fill((35,38,117))
+            self.screen.blit(self.background, (0,0))
             self.make_text(400, 150, 'PIXEL SERPENT', color = (255,255,255), size = 150, a = True)
             for event in pg.event.get():
                 if event.type == pg.QUIT:

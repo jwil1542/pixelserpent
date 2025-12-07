432e1d5b98a1a2028e90bbc611fe239488e5ac86
jasmyn williams <jwil1542@students.kennesaw.edu>
2025-12-07 03:04:36 -0600

    Change window title and background text color

--- correctsnakegame.py
+++ correctsnakegame.py
@@ -75,17 +75,9 @@ class apple():
 class game():
     def __init__(self,speed, size=1):
         self.screen = pg.display.set_mode((800,450))
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         self.background = pg.image.load("menu_background.jpg").convert()
         self.background = pg.transform.scale(self.background, (800, 450))
-        pg.display.set_caption('Snake Game')
-=======
         pg.display.set_caption('Pixel Serpent')
->>>>>>> Stashed changes
-=======
-        pg.display.set_caption('Pixel Serpent')
->>>>>>> Stashed changes
         self.snake = snake(speed, size)
         self.blocks = []
         self.score = 0
@@ -301,18 +293,10 @@ class startmenu():
         self.make_text((pos[0]+pos[2]/2), (pos[1]+pos[3]/2), text, a = True, size=textsize)
     def mainloop(self):
         while 1:
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
             self.screen.blit(self.background, (0,0))
             self.make_text(400, 150, 'PIXEL SERPENT', color = (255,255,255), size = 150, a = True)
-=======
-            self.screen.fill((255,189,210))
-            self.make_text(400, 150, 'PIXEL SERPENT', color = (46,20,5), size = 100, a = True)
->>>>>>> Stashed changes
-=======
-            self.screen.fill((255,189,210))
-            self.make_text(400, 150, 'PIXEL SERPENT', color = (46,20,5), size = 100, a = True)
->>>>>>> Stashed changes
+
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     sys.exit()
